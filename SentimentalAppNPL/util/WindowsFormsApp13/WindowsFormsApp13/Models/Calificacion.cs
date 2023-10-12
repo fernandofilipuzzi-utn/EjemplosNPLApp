679a0f2380f65656cc00087fa6168f4f466643ce
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2023-10-11 21:33:37 -0300

    actualizando

---
+++ SentimentalAppNPL/util/WindowsFormsApp13/WindowsFormsApp13/Models/Calificacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp13.Models
+{
+    class Calificacion
+    {
+        public int Puntuacion { get; set; }
+        public string Contenido { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Contenido};{Puntuacion} ";
+        }
+    }
+}
