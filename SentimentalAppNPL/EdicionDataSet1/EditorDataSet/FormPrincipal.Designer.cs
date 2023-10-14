a6f7edf8aa4d4330bc936459f6f5ceb85d44d7cf
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2023-10-13 21:20:57 -0300

    limpiando proyecto

---
+++ SentimentalAppNPL/EdicionDataSet1/EditorDataSet/FormPrincipal.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace EditorDataSet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbComentario = new System.Windows.Forms.TextBox();
+            this.btnAgregarValoracion = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnExportar = new System.Windows.Forms.Button();
+            this.nupValoracion = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nupValoracion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbComentario
+            // 
+            this.tbComentario.Location = new System.Drawing.Point(75, 65);
+            this.tbComentario.Multiline = true;
+            this.tbComentario.Name = "tbComentario";
+            this.tbComentario.Size = new System.Drawing.Size(475, 170);
+            this.tbComentario.TabIndex = 0;
+            // 
+            // btnAgregarValoracion
+            // 
+            this.btnAgregarValoracion.Location = new System.Drawing.Point(556, 19);
+            this.btnAgregarValoracion.Name = "btnAgregarValoracion";
+            this.btnAgregarValoracion.Size = new System.Drawing.Size(111, 55);
+            this.btnAgregarValoracion.TabIndex = 2;
+            this.btnAgregarValoracion.Text = "Agregar Valoración";
+            this.btnAgregarValoracion.UseVisualStyleBackColor = true;
+            this.btnAgregarValoracion.Click += new System.EventHandler(this.btnAgregarValoracion_Click);
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.Location = new System.Drawing.Point(556, 245);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(111, 51);
+            this.btnExportar.TabIndex = 4;
+            this.btnExportar.Text = "Exportar todas las valoraciones";
+            this.btnExportar.UseVisualStyleBackColor = true;
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
+            // 
+            // nupValoracion
+            // 
+            this.nupValoracion.Location = new System.Drawing.Point(75, 14);
+            this.nupValoracion.Name = "nupValoracion";
+            this.nupValoracion.Size = new System.Drawing.Size(120, 20);
+            this.nupValoracion.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Valoración";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Comentario";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 301);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nupValoracion);
+            this.Controls.Add(this.btnExportar);
+            this.Controls.Add(this.btnAgregarValoracion);
+            this.Controls.Add(this.tbComentario);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Editor y Exportador del dataset";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nupValoracion)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbComentario;
+        private System.Windows.Forms.Button btnAgregarValoracion;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnExportar;
+        private System.Windows.Forms.NumericUpDown nupValoracion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SentimentalAppNPL/EdicionDataSet1/EditorDataSet/FormPrincipal.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EditorDataSet.Models;
+
+namespace EditorDataSet
+{
+    public partial class Form1 : Form
+    {
+        List<Calificacion> calificaciones = new List<Calificacion>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = Application.StartupPath;
+            FileStream fs = null;
+            BinaryFormatter bf = new BinaryFormatter();
+
+            try
+            {
+                fs = new FileStream(Path.Combine(path, "datos.dat"), FileMode.Open, FileAccess.Read);
+                calificaciones = bf.Deserialize(fs) as List<Calificacion>;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (fs != null) fs.Close();
+            }
+
+            if (calificaciones == null)
+                calificaciones = new List<Calificacion>();
+        }
+
+        private void btnAgregarValoracion_Click(object sender, EventArgs e)
+        {
+            int puntuacion = Convert.ToInt32(nupValoracion.Value);
+            string contenido = tbComentario.Text.Replace("\r", "").Replace("\n", "").Replace("\t", "");
+
+            calificaciones.Add(new Calificacion
+            {
+                Puntuacion = puntuacion,
+                Contenido = contenido
+            });
+
+            nupValoracion.Value = 0;
+            tbComentario.Clear();
+        }
+
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Exportando las críticas";
+            saveFileDialog1.Filter = "Fichero json|*.json";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = null;
+                StreamWriter sw = null;
+
+                try
+                {
+                    fs = new FileStream(saveFileDialog1.FileName, FileMode.Create, FileAccess.Write);
+                    sw = new StreamWriter(fs);
+
+                    string pathDir = Path.GetDirectoryName(saveFileDialog1.FileName);
+
+                    sw.WriteLine("[");
+
+                    int n = 0;
+                    foreach (Calificacion c in calificaciones)
+                    {
+                        string contenido = c.Contenido.Replace("\"", "\\\"").Replace("\r", "").Replace("\n", "").Replace("(", "").Replace("\n", "").Replace(")", "");
+                        contenido = contenido.Length > 500 ? contenido.Substring(0, 500) : contenido;
+                        sw.WriteLine($"{{\"comentario\" : \"{contenido}\",\"valoracion\":{c.Puntuacion}}}");
+                        n++;
+                        if (n < calificaciones.Count) sw.Write(',');
+                    }
+                    sw.WriteLine("]");
+                    sw.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    if (fs != null)
+                    {
+                        if (sw != null) sw.Close();
+                        fs.Close();
+                        fs.Dispose();
+                    }
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string path = Application.StartupPath;
+            FileStream fs = null;
+            BinaryFormatter bf = new BinaryFormatter();
+
+            try
+            {
+                fs = new FileStream(Path.Combine(path, "datos.dat"), FileMode.Create,
+                                                                       FileAccess.Write);
+                bf.Serialize(fs, calificaciones);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+}
